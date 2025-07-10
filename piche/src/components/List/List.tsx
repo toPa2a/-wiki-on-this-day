457054338ab93a84e3a8a0b8e29f4db8b00fb603
Andrii <topa2a.xtbh@gmail.com>
2025-07-10 16:06:26 +0300

    feat(loader): added to app,added animations

--- piche/src/components/List/List.tsx
+++ piche/src/components/List/List.tsx
@@ -1,8 +1,12 @@
 import { useSelector } from 'react-redux';
 
 import ListItem from '../ListItem/ListItem';
+import Loader from '../Loader/Loader';
 import { getEvents } from '@/reducers/this-day';
+import { getStatus } from '@/reducers/status';
 import type { Items, Item } from '@/types/items';
+
+import './List.css';
 
 const eventTypes = [
   'selected',
@@ -14,10 +18,12 @@ const eventTypes = [
 
 const List = () => {
   const events = useSelector(getEvents) as Items;
+  const status = useSelector(getStatus);
   const isEmpty = Object.keys(events).length === 0;
 
   return !isEmpty && (
     <div className="events-list">
+      {status === 'loading' && <Loader />}
       {eventTypes.map(type => events[type].length > 0 && (
         <div key={type}>
           <h2>{type.toUpperCase()}</h2>
---
+++ piche/src/components/Loader/Loader.tsx
@@ -0,0 +1,12 @@
+import './Loader.css';
+
+const Loader = () => {
+  return (
+    <div className='loader'>
+      <div className="loader__animation"></div>
+      Loading...
+    </div>
+  );
+};
+
+export default Loader;
